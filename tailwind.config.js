e144a7210290f55fb99b14e895437d15f53be5cf
IM-san <ibrahimattiasan@gmail.com>
2022-02-08 09:44:52 -0800

    init tailwindcss and add the fonts

---
+++ tailwind.config.js
@@ -0,0 +1,102 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: { 
+      'amatic':['"Amatic SC"', 'serif'],
+      'cabin':['Cabin', 'serif'],
+      'fira':['"Fira Sans"', 'serif'],
+      'ibm':['"IBM Plex Sans Thai Looped"', 'serif'],
+      'imperial':['"Imperial Script"', 'serif'],
+      'inconsolata':['Inconsolata', 'serif'],
+      'lato':['Lato', 'serif'],
+      'libre128':['"Libre Barcode 128 Text"', 'serif'],
+      'libre39':['"Libre Barcode 39 Text"', 'serif'],
+      'libreBaskerville':['"Libre Baskerville"', 'serif'],
+      'merriweather':['Merriweather', 'serif'],
+      'merriweathersan':['"Merriweather Sans"', 'serif'],
+      'montserrat':['Montserrat', 'serif'],
+      'montserratalt':['"Montserrat Alternates"', 'serif'],
+      'mukta':['Mukta', 'serif'],
+      'nunito':['Nunito', 'serif'],
+      'nunitosan':['"Nunito Sans"', 'serif'],
+      'opensans':['"Open Sans"', 'serif'],
+      'opensanscon':['"Open Sans Condensed"', 'serif'],
+      'oxygen':['Oxygen', 'serif'],
+      'pfd':['"Playfair Display"', 'serif'],
+      'pfdsc':['"Playfair Display SC"', 'serif'],
+      'poppins':['Poppins', 'serif'],
+      'roboto':['Roboto', 'serif'],
+      'rubik':['Rubik', 'serif'],
+      'sourcesan3':['"Source Sans 3"', 'serif'],
+      'sourcesanpro':['"Source Sans Pro"', 'serif'],
+      'teko':['Teko', 'serif'],
+      'tiliweb':['"Titillium Web"', 'serif'],
+      'ubuntu':['Ubuntu', 'serif'],
+      'worksans':['"Work Sans"', 'serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+ /*    font-family: 'Amatic SC', cursive;
+
+    font-family: 'Cabin', sans-serif;
+
+    font-family: 'Fira Sans', sans-serif;
+
+    font-family: 'IBM Plex Sans Thai Looped', sans-serif;
+
+    font-family: 'Imperial Script', cursive;
+
+    font-family: 'Inconsolata', monospace;
+
+    font-family: 'Lato', sans-serif;
+
+    font-family: 'Libre Barcode 128 Text', cursive;
+
+    font-family: 'Libre Barcode 39 Text', cursive;
+
+    font-family: 'Libre Baskerville', serif;
+
+    font-family: 'Merriweather', serif;
+
+    font-family: 'Merriweather Sans', sans-serif;
+
+    font-family: 'Montserrat', sans-serif;
+
+    font-family: 'Montserrat Alternates', sans-serif;
+
+    font-family: 'Mukta', sans-serif;
+
+    font-family: 'Nunito', sans-serif;
+
+    font-family: 'Nunito Sans', sans-serif;
+
+    font-family: 'Open Sans', sans-serif;
+
+    font-family: 'Open Sans Condensed', sans-serif;
+
+    font-family: 'Oxygen', sans-serif;
+
+    font-family: 'Playfair Display', serif;
+
+    font-family: 'Playfair Display SC', serif;
+
+    font-family: 'Poppins', sans-serif;
+
+    font-family: 'Roboto', sans-serif;
+
+    font-family: 'Rubik', sans-serif;
+
+    font-family: 'Source Sans 3', sans-serif;
+
+    font-family: 'Source Sans Pro', sans-serif;
+
+    font-family: 'Teko', sans-serif;
+
+    font-family: 'Titillium Web', sans-serif;
+
+    font-family: 'Ubuntu', sans-serif;
+
+    font-family: 'Work Sans', sans-serif; */
